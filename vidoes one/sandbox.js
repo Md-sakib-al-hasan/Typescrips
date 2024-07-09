5d3e626b2a587acc74f7f0880e02daeaadcf7913
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2024-07-09 16:26:22 +0600

    any

---
+++ vidoes one/sandbox.js
@@ -0,0 +1,5 @@
+var sakar;
+sakar: "sk";
+sakar: 12;
+var mixed = [];
+var narji;
--- vidoes one/sandbox.ts
+++ vidoes one/sandbox.ts
@@ -1,1 +1,9 @@
+let sakar: any;
+ sakar:"sk"
+ sakar:12;
 
+ let mixed: any[] = [];
+ let narji:{
+    name:any,
+    age:any
+ }
