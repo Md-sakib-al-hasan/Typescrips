18fb4d7230b2a6d1b283938827845541f7fdd62e
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2024-07-09 15:42:26 +0600

    four

--- vidoes one/sandbox.js
+++ vidoes one/sandbox.js
@@ -1,4 +1,13 @@
-var character = 'mario';
-var names = 'kan';
-names = 'sk';
-console.log(names);
+var names = ['luigi', 'mario', 'yoshi', 2, 4];
+names.push('sk');
+names.push(7);
+// names.push(true)
+var ninar = {
+    name: 'sakib',
+    age: 45
+};
+// ninar = {
+//     name:'sk',
+//     age:33,
+//     skills:"sk"
+// }
--- vidoes one/sandbox.ts
+++ vidoes one/sandbox.ts
@@ -1,7 +1,18 @@
-let character = 'mario';
-let names  = 'kan';
+let names = ['luigi','mario','yoshi',2,4]
 
- names = 'sk';
+names.push('sk');
+names.push(7);
 
+// names.push(true)
 
-console.log(names)
+let ninar = {
+    name: 'sakib',
+    age: 45
+}
+
+// ninar = {
+//     name:'sk',
+//     age:33,
+//     skills:"sk"
+// }
+
