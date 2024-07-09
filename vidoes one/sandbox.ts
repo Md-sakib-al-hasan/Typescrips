080b8ff30dfe6f011476a363f09eff1ff417e4f0
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2024-07-09 16:21:48 +0600

    any

--- vidoes one/sandbox.ts
+++ vidoes one/sandbox.ts
@@ -1,18 +1,1 @@
-let names = ['luigi','mario','yoshi',2,4]
-
-names.push('sk');
-names.push(7);
-
-// names.push(true)
-
-let ninar = {
-    name: 'sakib',
-    age: 45
-}
-
-// ninar = {
-//     name:'sk',
-//     age:33,
-//     skills:"sk"
-// }
 
